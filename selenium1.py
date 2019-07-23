2f603aba536f6bd5db546de27fe79a6ae354e410
Poppy-Wang <15829765307@163.com>
2019-07-23 10:25:25 +0800

    selenium1

--- selenium1.py
+++ selenium1.py
@@ -1,14 +1,9 @@
-#coding=utf-8
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
+
+
 driver=webdriver.Chrome()
 driver.get('https://www.baidu.com')
-driver.find_element_by_id('kw').send_keys('selenium')
-driver.find_element_by_id('su').click()
+driver.find_element_by_id("kw").clear()
+driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("su").click()
 driver.quit()
-
-
-
-
